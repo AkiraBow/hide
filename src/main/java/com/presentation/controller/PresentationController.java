a78b6a8a1b630128509862ebfbaefc907385582c
Chandler <chandler.zhu@ext.airbnb.com>
2020-04-05 15:40:33 -0400

    add k8s yaml files logs

--- src/main/java/com/presentation/controller/PresentationController.java
+++ src/main/java/com/presentation/controller/PresentationController.java
@@ -1,5 +1,6 @@
 package com.presentation.controller;
 
+import lombok.extern.slf4j.Slf4j;
 import org.apache.http.client.fluent.Content;
 import org.apache.http.client.fluent.Request;
 import org.springframework.beans.factory.annotation.Value;
@@ -11,15 +12,18 @@ import java.io.IOException;
 
 
 @Controller
+@Slf4j
 public class PresentationController {
   @Value("${k8s.Bayonetta}")
   private String SERVICE_NAME;
 
   @GetMapping("/allbooks")
   public String findAll(Model model) throws IOException {
+    log.info("Request URL:"+SERVICE_NAME+"/api/books");
     Content content = Request.Get(SERVICE_NAME+"/api/books")
       .execute().returnContent();
 
+    log.info("Json from backend:"+content.asString());
     model.addAttribute("booksJson", content.asString());
     return "book";
   }
